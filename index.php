42c31f231a30c2a31c26d4f18f4f6fc273fa9813
Your Name <your-email@example.com>
2025-05-19 08:49:20 +0300

    Noon site copying - header

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Noon</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+         <img src="https://f.nooncdn.com/s/app/com/noon/design-system/logos/noon-logo-en.svg" alt="">
+         <button class="to-dubai">
+            <img id="flag" src="https://f.nooncdn.com/s/app/com/common/images/flags/ae.svg" alt="">
+            <div class="underline">
+                <span id="deliver">Deliver to</span>
+                <span id="dubai">Dubai
+                 <img id="down" src="https://f.nooncdn.com/s/app/com/noon/icons/down-chevron.svg" alt="">
+                </span>
+
+            </div>
+         </button>
+         <input type="text" placeholder="What are you looking for?">
+         <button id="arabic">العربية</span>
+         <button class="login">Log in
+            <img id="user" src="https://f.nooncdn.com/s/app/com/noon/icons/user.svg" alt="">
+         </button>
+         <button class="wishlist"><img src="https://f.nooncdn.com/s/app/com/noon/icons/wishlist-dark.svg" alt=""></button>
+         <button class="cart"><img src="https://f.nooncdn.com/s/app/com/noon/icons/cart_2.svg" alt=""></button>
+        </div>
+    </header>
+    <nav>
+        
+    </nav>
+</body>
+</html>
